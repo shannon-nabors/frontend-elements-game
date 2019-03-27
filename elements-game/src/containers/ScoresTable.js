dbd6030997735a9f7e5140350ea83985c7ac22b5
Anthony Gregg <agregg22@gmail.com>
2019-03-26 21:05:40 -0400

    styled scores table

--- elements-game/src/containers/ScoresTable.js
+++ elements-game/src/containers/ScoresTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
 import { Table } from 'semantic-ui-react';
-import TableRow from '../components/TableRow'
+import TableRow from '../components/TableRow';
 
 class ScoresTable extends Component {
   render() {
@@ -20,21 +20,29 @@ class ScoresTable extends Component {
             <Table celled>
               <Table.Header>
                 <Table.Row>
-                  <Table.HeaderCell>Name</Table.HeaderCell>
-                  <Table.HeaderCell>Mode</Table.HeaderCell>
-                  <Table.HeaderCell>Correct</Table.HeaderCell>
-                  <Table.HeaderCell>Total</Table.HeaderCell>
-                  <Table.HeaderCell>% Correct</Table.HeaderCell>
+                  <Table.HeaderCell className="table-header">
+                    Name
+                  </Table.HeaderCell>
+                  <Table.HeaderCell className="table-header">
+                    Mode
+                  </Table.HeaderCell>
+                  <Table.HeaderCell className="table-header">
+                    Correct
+                  </Table.HeaderCell>
+                  <Table.HeaderCell className="table-header">
+                    Total
+                  </Table.HeaderCell>
+                  <Table.HeaderCell className="table-header">
+                    % Correct
+                  </Table.HeaderCell>
                 </Table.Row>
               </Table.Header>
 
               <Table.Body>
-                {this.props.currentScores.map( (score, idx) => 
-                  (<TableRow key={idx} score={score} 
-                    user={this.props.user}
-                    />)
-                  )}
-                </Table.Body>
+                {this.props.currentScores.map((score, idx) => (
+                  <TableRow key={idx} score={score} user={this.props.user} />
+                ))}
+              </Table.Body>
             </Table>
           </div>
           <div className="ui two wide column" />
